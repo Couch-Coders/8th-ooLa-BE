f94c5e6db9bc7cee455600e060807c514aea9e86
meeyoungchoi <goodlife1359@gmail.com>
2022-05-21 22:59:32 +0900

    initialize repository

---
+++ src/main/java/com/couchcoding/studyoola/StudyoolaApplication.java
@@ -0,0 +1,13 @@
+package com.couchcoding.studyoola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyoolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyoolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/couchcoding/studyoola/StudyoolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couchcoding.studyoola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyoolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
