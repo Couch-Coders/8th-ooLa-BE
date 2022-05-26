361057f623196ebc8a755fc80a00046a3d8af0db
meeyoungchoi <goodlife1359@gmail.com>
2022-05-26 14:52:07 +0900

    FEATURE:Firebase 프로젝트 생성 및 spring security 관련 설정

---
+++ src/main/java/com/couchcoding/oola/config/FirebaseInitializer.java
@@ -0,0 +1,42 @@
+package com.couchcoding.oola.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+// Firebase Auth 인증 관련 모듈 초기화
+@Slf4j
+@Configuration
+public class FirebaseInitializer {
+
+    // FirebaseApp을 초기화 한다
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        log.info("Initializing Firebase.");
+        FileInputStream serviceAccount = new FileInputStream("firebase.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setStorageBucket("heroku-sample.appspot.com")
+                .build();
+
+        FirebaseApp app = FirebaseApp.initializeApp(options);
+        log.info("FirebaseApp initialized: {}" , app.getName());
+
+        return app;
+    }
+
+    // FirebaseAuth를 초기화 한다
+    @Bean
+    public FirebaseAuth getFirebaseAuth() throws IOException {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance(firebaseApp());
+        return firebaseAuth;
+    }
+}
