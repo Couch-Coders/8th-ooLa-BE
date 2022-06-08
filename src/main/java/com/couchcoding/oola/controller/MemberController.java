fd3c81d2faa4795f3a418b25072a5000511b8178
meeyoungchoi <goodlife1359@gmail.com>
2022-06-08 19:19:47 +0900

    FEATURE: 로그인시 member 정보가 없는경우 404 던지도록 처리

--- src/main/java/com/couchcoding/oola/controller/MemberController.java
+++ src/main/java/com/couchcoding/oola/controller/MemberController.java
@@ -68,6 +68,9 @@ public class MemberController {
     @GetMapping("/me")
     public ResponseEntity<MemberResponseDto> login(Authentication authentication) {
         Member member = ((Member) authentication.getPrincipal());
+        if (member.equals(null)) {
+            return (ResponseEntity<MemberResponseDto>) ResponseEntity.notFound();
+        }
         log.info("member: {}", member.toString());
         return ResponseEntity.ok(new MemberResponseDto(member));
     }
