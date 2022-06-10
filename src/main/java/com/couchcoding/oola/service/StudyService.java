a302b360687db0c964e91553f4ca19fafdabdf4a
meeyoungchoi <goodlife1359@gmail.com>
2022-06-10 11:21:03 +0900

    FEATURE: 서버 정상적으로 올라오는지 확인

--- src/main/java/com/couchcoding/oola/service/StudyService.java
+++ src/main/java/com/couchcoding/oola/service/StudyService.java
@@ -19,6 +19,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
 @Slf4j
 @RequiredArgsConstructor
 @Service
