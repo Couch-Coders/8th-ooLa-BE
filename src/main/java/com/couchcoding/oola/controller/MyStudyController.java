f092907cf5145ceff98c440c4fb31e90ab8ba529
meeyoungchoi <goodlife1359@gmail.com>
2022-06-11 20:05:10 +0900

    FEATURE: 마이스터디 관련 요청을 처리하기 위한 컨트롤러 추가

---
+++ src/main/java/com/couchcoding/oola/controller/MyStudyController.java
@@ -0,0 +1,9 @@
+package com.couchcoding.oola.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mystudies")
+public class MyStudyController {
+}
