5c17ce98bd8a572d2cd03e12a094fef2ddffaa56
meeyoungchoi <goodlife1359@gmail.com>
2022-06-11 23:08:26 +0900

    FEATURE: 마이프로필 조회 에러 수정

---
+++ src/main/java/com/couchcoding/oola/repository/impl/MemberRepositoryImpl.java
@@ -0,0 +1,43 @@
+package com.couchcoding.oola.repository.impl;
+
+import com.couchcoding.oola.entity.Member;
+import com.couchcoding.oola.entity.QMember;
+import com.couchcoding.oola.entity.Study;
+import com.couchcoding.oola.repository.MemberRepositoryCustom;
+import com.couchcoding.oola.repository.StudyRepositoryCustom;
+import com.querydsl.core.types.dsl.BooleanExpression;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.stereotype.Repository;
+
+import static com.couchcoding.oola.entity.QMember.*;
+
+
+@Repository
+public class MemberRepositoryImpl extends QuerydslRepositorySupport implements MemberRepositoryCustom {
+
+    @Autowired
+    private JPAQueryFactory queryFactory;
+
+    public MemberRepositoryImpl() {
+        super(Member.class);
+
+    }
+
+    @Override
+    public Member findByUid(String uid) {
+     Member member = queryFactory.selectFrom(QMember.member)
+                .where(eqUid(uid)).fetchOne();
+        return member;
+    }
+
+
+    private BooleanExpression eqUid(String uid) {
+        if (uid == null || uid.isEmpty()) {
+            return null;
+        }
+        return member.uid.eq(uid);
+    }
+}
